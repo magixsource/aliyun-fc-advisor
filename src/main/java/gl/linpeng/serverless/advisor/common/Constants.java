08127a1c88246982dc0d4dd5ece89ba9acb3b6cd
lin.peng <lin.peng@dtyunxi.com>
2020-02-29 20:30:02 +0800

    增加枚举类

--- src/main/java/gl/linpeng/serverless/advisor/common/Constants.java
+++ src/main/java/gl/linpeng/serverless/advisor/common/Constants.java
@@ -126,4 +126,94 @@ public final class Constants {
 
     }
 
+    public enum OperationLogType {
+
+        DOWN(0, "DOWN"), UP(1, "UP"), VISIT(2, "VISIT"), SHARE(3, "SHARE");
+
+        public String name;
+        public Integer value;
+
+        OperationLogType(Integer value, String name) {
+            this.name = name;
+            this.value = value;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public Integer getValue() {
+            return value;
+        }
+
+        public void setValue(Integer value) {
+            this.value = value;
+        }
+
+    }
+
+    public enum OperationLogTargetType {
+
+        PRINCIPLE(1, "PRINCIPLE"), ARTICLE(2, "ARTICLE"), FOOD(3, "FOOD");
+
+        public String name;
+        public Integer value;
+
+        OperationLogTargetType(Integer value, String name) {
+            this.name = name;
+            this.value = value;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public Integer getValue() {
+            return value;
+        }
+
+        public void setValue(Integer value) {
+            this.value = value;
+        }
+
+    }
+
+    public enum OperationLogSourceType {
+
+        HUMAN(1, "HUMAN"), ARTICLE(2, "ARTICLE");
+
+        public String name;
+        public Integer value;
+
+        OperationLogSourceType(Integer value, String name) {
+            this.name = name;
+            this.value = value;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public Integer getValue() {
+            return value;
+        }
+
+        public void setValue(Integer value) {
+            this.value = value;
+        }
+
+    }
+
 }
